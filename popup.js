f255024ef25d6f99dd362e653f92b16241fa626c
Marcelo Tapia <t.marcelo.p@gmail.com>
2016-05-31 22:35:54 -0400

    Added server indexing functionality.

--- popup.js
+++ popup.js
@@ -1,118 +1,87 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var regions = ['NA', 'EU', 'AS', 'SA', 'OC'];
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+var regionTable = {
+    'NA': ['US'],
+    'EU': ['EU', 'RU', 'TK'],
+    'AS': ['JP', 'TK', 'SG', 'CN'],
+    'SA': ['BR'],
+	'OC': ['SG']
+};
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
+var regionNames = {
+	'NA': 'North America',
+    'EU': 'Europe',
+    'AS': 'Asia',
+    'SA': 'South America',
+	'OC': 'Oceania'
+}
+var serverList;
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+var options = {
+	'NA': '',
+    'EU': '',
+    'AS': '',
+    'SA': '',
+	'OC': ''
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
+$(function() {
+	$('#Combobox1').html('<option selected value="invalid">Obtaining Server List...</option>');
+	$('#Combobox2').html('<option selected value="invalid">Obtaining Server List...</option>');
+	
+	$('#Combobox1').prop("disabled", true);
+	$('#Combobox2').prop("disabled", true);
+	
+	$.getJSON('http://m.agar.io/fullInfo', serverListCallback);
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
-  });
+	$('#Combobox1').change(regionChangeEvent);
 });
+
+function serverListCallback(serverList) {
+	var regionCode = "";
+	var inRegion = false;
+	var region;
+	
+	// Sort alphabetically
+	serverList.servers.sort(function (a, b) {
+		if(a.region < b.region) return -1;
+		if(a.region > b.region) return 1;
+		return 0;
+	});
+	
+	$('#Combobox1').html('<option value="invalid">Select Region...</option>');
+	$('#Combobox2').html('<option value="invalid">Select Server...</option>');
+	
+	$.each(serverList.servers, function(index, server) {
+		regionCode = server.region.substring(0,2);
+		// Obtain region of server
+		for (var i in regions) {
+			inRegion = ($.inArray(regionCode, regionTable[regions[i]]) != -1)? true : false;
+			if (inRegion){
+				region = regions[i];
+			}
+		}
+		console.log(region);
+		// Check if region wasn't added before
+		if (!(~$('#Combobox1').html().indexOf(regionNames[region]))) {
+			// Add region as option
+			$('#Combobox1').append('<option value="' + region + '">' + regionNames[region] + '</option>');
+		}
+		
+		// Store server for region
+		if (server.isEnabled) {
+			options[region] += '<option value="' + server.ip + '">' + server.region.replace('-', ' ') + ' (' + server.ip + ') (' + server.numPlayers + ' players)</option>';
+		}
+	});
+	
+	$('#Combobox1').prop("disabled", false);
+	$('#Combobox2').prop("disabled", false);
+}
+
+function regionChangeEvent() {
+	var region =  $(this).find('option:selected').val();
+	if (region == null) return;
+	console.log(region);
+	$('#Combobox2').html('<option value="invalid">Select Server...</option>');
+	$('#Combobox2').append(options[region]);
+}
